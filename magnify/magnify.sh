d0aa36396c34e65c4b07c4c5ee93c89781c74447
manuk <mahhov1@gmail.com>
2018-07-24 09:02:28 -0700

    kill option

--- magnify/magnify.sh
+++ magnify/magnify.sh
@@ -3,25 +3,30 @@ SIZE=300;
 
 dir=$(dirname "$0")
 
-mouse=$(xdotool getmouselocation | tr ':' ' ')
-screen=$(xdotool getdisplaygeometry)
-x=$(echo $mouse | cut -d ' ' -f 2)
-y=$(echo $mouse | cut -d ' ' -f 4)
-width=$(echo $screen | cut -d ' ' -f 1)
-height=$(echo $screen | cut -d ' ' -f 2)
+if [ "$1" == kill ] ; then
+    pkill -9 -f "display $dir/temp.png"
+   
+else
+    mouse=$(xdotool getmouselocation | tr ':' ' ')
+    screen=$(xdotool getdisplaygeometry)
+    x=$(echo $mouse | cut -d ' ' -f 2)
+    y=$(echo $mouse | cut -d ' ' -f 4)
+    width=$(echo $screen | cut -d ' ' -f 1)
+    height=$(echo $screen | cut -d ' ' -f 2)
 
-clip() {
-    value=$1
-    min=$2
-    max=$3
-    value=$((value<min?min:value))
-    value=$((value>max?max:value))
-    echo $value
-}
+    clip() {
+        value=$1
+        min=$2
+        max=$3
+        value=$((value<min?min:value))
+        value=$((value>max?max:value))
+        echo $value
+    }
 
-left=$(clip $((x-SIZE/2)) 0 $((width-SIZE)))
-top=$(clip $((y-SIZE/2)) 0 $((height-SIZE)))
+    left=$(clip $((x-SIZE/2)) 0 $((width-SIZE)))
+    top=$(clip $((y-SIZE/2)) 0 $((height-SIZE)))
 
-import -window root -crop ${SIZE}x$SIZE+$left+$top -resize $ZOOM $dir/temp.png
-display $dir/temp.png &
+    import -window root -crop ${SIZE}x$SIZE+$left+$top -resize $ZOOM $dir/temp.png
+    display $dir/temp.png &
+fi
 
