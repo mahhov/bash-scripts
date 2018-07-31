73ab76f750305d48d9ed09af4e26c2db694bddea
manuk <mahhov1@gmail.com>
2018-07-31 08:34:35 -0700

    prefix files/ to file naem in build file

--- readDevInput/appendBuildFile.js
+++ readDevInput/appendBuildFile.js
@@ -5,7 +5,7 @@ let header = process.argv[3];
 let files = process.argv[4];
 
 let tab = '    ';
-let append = files.split('\n').map(file => `\n${tab}"${file}",`).reduce((a, b) => `${a}${b}`, '');
+let append = files.split('\n').map(file => `\n${tab}"files/${file}",`).reduce((a, b) => `${a}${b}`, '');
 
 console.log(body.replace(header, header + append));
 
