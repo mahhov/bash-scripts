ecb8a6b45504861e4b94f0dcd8da632de88e1e3f
manuk <mahhov1@gmail.com>
2018-07-31 08:31:37 -0700

    use space insetad of tab or else ninja build complains

--- readDevInput/appendBuildFile.js
+++ readDevInput/appendBuildFile.js
@@ -4,7 +4,8 @@ let body = process.argv[2];
 let header = process.argv[3];
 let files = process.argv[4];
 
-let append = files.split('\n').map(file => `\n\t"${file}",`).reduce((a, b) => `${a}${b}`, '');
+let tab = '    ';
+let append = files.split('\n').map(file => `\n${tab}"${file}",`).reduce((a, b) => `${a}${b}`, '');
 
 console.log(body.replace(header, header + append));
 
