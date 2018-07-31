2832cbb9640e9a21b7364642a4fa013d495d9d93
manuk <mahhov1@gmail.com>
2018-07-31 08:27:00 -0700

    read dev input print out useage examples

--- readDevInput/setup.sh
+++ readDevInput/setup.sh
@@ -2,3 +2,10 @@ src=~/workspace/chromium/src
 cp src/* $src/base/files/
 touch $src/read.txt
 ./appendBuildFile.js "$(cat $src/base/BUILD.gn)" "sources = [" "$(ls src)" > $src/base/BUILD.gn
+
+echo '#include "base/files/read_dev_input.h"'
+echo 'ReadDevInput x("read.txt");'
+echo 'x.get("myKey");'
+echo 'x.get("myKey", 0);'
+echo 'x.getList("myKey")[0];'
+
