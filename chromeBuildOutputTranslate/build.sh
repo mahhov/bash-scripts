d2b0cf2647a077d2f67aeefd9b3517301aa53bd9
manuk <mahhov1@gmail.com>
2018-09-05 06:54:35 -0700

    chrome run & build scripts to take optional param for build name

--- chromeBuildOutputTranslate/build.sh
+++ chromeBuildOutputTranslate/build.sh
@@ -2,8 +2,10 @@ export PATH=$PATH:~/.nvm/versions/node/v10.3.0/bin # this sucks but intellij is 
 
 dir=$(dirname $(realpath "$0"))
 
+buildName=`[ -z $1 ] && echo out/Default || echo $1`
+
 pushd ~/workspace/chromium/src
-unbuffer ~/workspace/depot_tools/ninja -C out/Default -j 1000 chrome | node $dir/translatePaths.js
+unbuffer ~/workspace/depot_tools/ninja -C $buildName -j 1000 chrome | node $dir/translatePaths.js
 status=${PIPESTATUS[0]}
 popd
 
--- chromeBuildOutputTranslate/run.sh
+++ chromeBuildOutputTranslate/run.sh
@@ -2,8 +2,10 @@ export PATH=$PATH:~/.nvm/versions/node/v10.3.0/bin
 
 dir=$(dirname $(realpath "$0"))
 
+buildName=`[ -z $1 ] && echo out/Default || echo $1`
+
 pushd ~/workspace/chromium/src
-unbuffer out/Default/chrome | $dir/translateRunColors.js
+unbuffer $buildName/chrome | $dir/translateRunColors.js
 status=${PIPESTATUS[0]}
 popd
 
